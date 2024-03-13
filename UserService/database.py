a4ae75fc95176ed6daf53b4b9310fc4a90c7c200
Migas77 <miguel.belchior@ua.pt>
2024-03-13 16:00:28 +0000

    dotenv

--- UserService/database.py
+++ UserService/database.py
@@ -1,8 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "postgresql://propertease:password@localhost/user_db"
+load_dotenv()
+
+user = os.getenv("POSTGRES_USER")
+password = os.getenv("POSTGRES_PASSWORD")
+db = os.getenv("POSTGRES_DB")
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{user}:{password}@localhost/{db}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
