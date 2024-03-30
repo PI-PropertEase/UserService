de4edb7696c14d81dd545d38b7c9408ee9188803
Migas77 <miguel.belchior@ua.pt>
2024-03-30 10:28:30 +0000

    PTSE-43 publish pydantic object

--- UserService/routers/customer.py
+++ UserService/routers/customer.py
@@ -14,7 +14,7 @@ def create_user(user: UserBase = Depends(get_user), db: Session = Depends(get_db
     if crud.get_user_by_email(db, email=user.email):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered")
     db_user = crud.create_user(db=db, user=user)
-    publish_new_user(db_user)
+    publish_new_user(user)
     return db_user
 
 
