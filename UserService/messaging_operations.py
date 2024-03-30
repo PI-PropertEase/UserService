285e058c0af101ee4d01b2f8986e56f73195da45
Migas77 <miguel.belchior@ua.pt>
2024-03-30 13:35:45 +0000

    PTSE-43 publish message with new functions from project utils

--- UserService/messaging_operations.py
+++ UserService/messaging_operations.py
@@ -1,7 +1,7 @@
 import pika
 
 from ProjectUtils.MessagingService.queue_definitions import channel, EXCHANGE_NAME
-from ProjectUtils.MessagingService.schemas import create_message, UserMessage, UserType
+from ProjectUtils.MessagingService.schemas import to_json, MessageFactory, MessageType
 from UserService.schemas import UserBase
 
 
@@ -9,6 +9,6 @@ def publish_new_user(user: UserBase):
     channel.basic_publish(
         exchange=EXCHANGE_NAME,
         routing_key="",
-        body=create_message(UserMessage(UserType.CREATE_USER, user)),
+        body=to_json(MessageFactory.create_user_message(MessageType.USER_CREATE, user)),
         properties=pika.BasicProperties(delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE),
     )
